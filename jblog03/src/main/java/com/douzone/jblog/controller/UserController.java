aed07e124e5c0a2405bc7870ef48b4ec807b1ff2
Kim-woo-gyeong <lovelykwg@naver.com>
2020-03-17 17:10:17 +0900

    jblog login & logout & join & joinsuccess

---
+++ jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
@@ -0,0 +1,70 @@
+package com.douzone.jblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.douzone.jblog.service.UserService;
+import com.douzone.jblog.vo.UserVo;
+import com.douzone.security.Auth;
+import com.douzone.security.AuthUser;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/join",method=RequestMethod.GET)
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping(value="/join",method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping("/joinsuccess")
+	public String joinSuccess() {
+		return "user/joinsuccess";
+	}
+	
+	@RequestMapping(value="/login",method = RequestMethod.GET)
+	public String login() {
+		return "user/login";
+	}
+	
+//	@Auth
+//	@RequestMapping(value="/update", method=RequestMethod.GET)
+//	public String update(
+//			@AuthUser UserVo authUser, Model model) {
+//
+//		
+//		Long no = authUser.getNo();
+//		
+//		UserVo vo = userService.findUser(no);
+//		model.addAttribute("vo", vo);
+//		return "user/update";
+//	}
+	
+//	@Auth
+//	@RequestMapping(value="/update/{bool}", method=RequestMethod.POST)
+//	public String userUpdate(
+//			@PathVariable("bool") boolean bool, 
+//			Model model, 
+//			@AuthUser UserVo authUser, UserVo vo) {
+//
+//		vo.setNo(authUser.getNo());
+//		model.addAttribute("bool", bool);
+//		
+//		userService.update(vo);
+//		authUser.setName(vo.getName());
+//		return "user/joinsuccess";
+//	}
+}
