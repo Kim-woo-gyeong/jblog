9d59779be136e8815a09fb6b6aa80485a47e66fe
Kim-woo-gyeong <lovelykwg@naver.com>
2020-03-25 20:06:16 +0900

    change to java config #4 - fileupload

--- jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
+++ jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.douzone.jblog.config.web.FileUploadConfig;
+import com.douzone.jblog.config.web.MessageConfig;
 import com.douzone.jblog.config.web.MvcConfig;
 import com.douzone.jblog.config.web.SecurityConfig;
 
 @Configuration
 @ComponentScan({"com.douzone.jblog.controller"})
-@Import({MvcConfig.class, SecurityConfig.class})
+@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class, FileUploadConfig.class})
 public class WebConfig {
 
 }
